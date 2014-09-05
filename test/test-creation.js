e4bd87c9d270add3b55ed48f2b7613471bb21e4f
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2014-09-05 12:06:13 -0500

    updated generator test

--- test/test-creation.js
+++ test/test-creation.js
@@ -20,12 +20,42 @@ describe('aip-science-app generator', function () {
   it('creates expected files', function (done) {
     var expected = [
       // add files you expect to exist here.
-      '.jshintrc',
-      '.editorconfig'
+      'Gruntfile.js',
+      'package.json',
+      'bower.json',
+      'app/app.html',
+      'app/scripts/app.js',
+      'app/styles/app.css',
+      'app/app.html',
+      'app/scripts/app.js',
+      'app/styles/app.css',
+      'index.html',
+      'lib/Agave.js',
+      'lib/api-client-auth.html',
+      'lib/resources/apps',
+      'lib/resources/auth',
+      'lib/resources/clients',
+      'lib/resources/files',
+      'lib/resources/index.html',
+      'lib/resources/jobs',
+      'lib/resources/meta',
+      'lib/resources/monitors',
+      'lib/resources/notifications',
+      'lib/resources/postits',
+      'lib/resources/profiles',
+      'lib/resources/systems',
+      'lib/resources/track',
+      'lib/resources/transfers',
+      'lib/resources/transforms',
+      'lib/test-runner.js',
+      '.editorconfig',
+      '.gitignore',
+      '.gitattributes',
+      '.jshintrc'
     ];
 
     helpers.mockPrompt(this.app, {
-      'someOption': true
+      'libraries': ['includeBioJS']
     });
     this.app.options['skip-install'] = true;
     this.app.run({}, function () {
